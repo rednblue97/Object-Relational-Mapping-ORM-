81584d2b15624b7cc6f94fe9125d9e3fc8986e46
CaptureB <airdavid62@gmail.com>
2023-02-28 23:29:49 -0700

    Defined the columns for product.js done

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type:Datatypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Datatypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: Datatypes.INTEGER,
+      refrencs: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
