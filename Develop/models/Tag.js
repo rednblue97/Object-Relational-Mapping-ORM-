ca2e7e3a2c03af672e228932a698da8997e4a806
CaptureB <airdavid62@gmail.com>
2023-02-28 23:44:09 -0700

    defined columns for tag.js done

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_id: {
+      type: Datatype.STRING,
+    }
   },
   {
     sequelize,
