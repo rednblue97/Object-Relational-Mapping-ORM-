bebbc98034a4fb5342d832f604caa61adf697110
CaptureB <airdavid62@gmail.com>
2023-03-01 11:27:10 -0700

    Established associations for product,category,tag

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,25 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsToMany(Category, {
+  foreignKey: 'category_id'
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+ through: ProductTag,
+ foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
